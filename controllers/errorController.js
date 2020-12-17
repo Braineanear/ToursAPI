5e28b590353b21a31dad69d9702fc2290040663f
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2020-12-17 15:12:58 +0200

    Update errorController (Store Error in The Database)

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,15 @@
 const AppError = require('../utils/appError');
+const ErrorStack = require('../models/errorModel');
+
+const saveError = async (err) => {
+  const newError = await ErrorStack.create({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+  return newError.id;
+};
 
 const handleJWTError = () =>
   new AppError('Invalid token, Please login again!', 401);
@@ -13,58 +24,55 @@ const handleCastErrorDB = (err) => {
 
 const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  console.log(value);
-
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
-
   const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
+const sendErrorDev = async (err, req, res) => {
+  const errorId = await saveError(err);
+  return res.status(err.statusCode).json({
     status: err.status,
     error: err,
-    message: err.message,
+    message: `${err.message} (${errorId})`,
     stack: err.stack
   });
 };
 
-const sendErrorProd = (err, res) => {
-  // Operational, trusted error: send message to client
+const sendErrorProd = async (err, req, res) => {
+  // A) Operational, trusted error: send message to client
   if (err.isOperational) {
-    res.status(err.statusCode).json({
+    const errorId = await saveError(err);
+    return res.status(err.statusCode).json({
       status: err.status,
-      message: err.message
-    });
-
-    // Programming or other unknown error: don't leak error details
-  } else {
-    // 1) Log error
-    console.error('ERROR 💥', err);
-
-    // 2) Send generic message
-    res.status(500).json({
-      status: 'error',
-      message: 'Something went very wrong!'
+      message: `${err.message} (${errorId})`
     });
   }
+
+  // B) Programming or other unknown error: don't leak error details
+  // 1) Log error
+  console.error('ERROR 💥', err);
+  // 2) Send generic message
+  const errorId = await saveError(err);
+  return res.status(500).json({
+    status: 'error',
+    message: `Something went wrong! (${errorId})`
+  });
 };
 
 module.exports = (err, req, res, next) => {
-  // console.log(err.stack);
-
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
+    error.message = err.message;
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
@@ -72,6 +80,7 @@ module.exports = (err, req, res, next) => {
       error = handleValidationErrorDB(error);
     if (error.name === 'JsonWebTokenError') error = handleJWTError();
     if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
-    sendErrorProd(error, res);
+
+    sendErrorProd(error, req, res);
   }
 };
