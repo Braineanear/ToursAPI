14bad781c365653959d69fa6ef854d072d0fa31f
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2020-12-17 15:19:20 +0200

    Update userModel

--- models/userModel.js
+++ models/userModel.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
-//name, email, photo, password, passwordConfim
-
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -41,28 +39,34 @@ const userSchema = new mongoose.Schema({
     }
   },
   passwordChangedAt: Date,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-  active: {
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  confirmEmailToken: String,
+  isEmailConfirmed: {
     type: Boolean,
-    default: true,
-    select: false
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 });
 
+// Encrypt password using bcrypt
 userSchema.pre('save', async function (next) {
-  //Only run this function if password was actually modified
+  // 1) Only run this function if password was actually modified
   if (!this.isModified('password')) return next();
 
-  //Hash the password with cost of 12
+  // 2) Salt & Hashing Password
   const salt = bcrypt.genSaltSync(12);
   this.password = await bcrypt.hashSync(this.password, salt);
 
-  //Delete passwordConfirm field
+  // 3) Delete passwordConfirm field
   this.passwordConfirm = undefined;
   next();
 });
 
+// Set passwordChangedAt field to the current time when the user change the password
 userSchema.pre('save', function (next) {
   if (!this.isModified('password') || this.isNew) return next();
 
@@ -70,21 +74,12 @@ userSchema.pre('save', function (next) {
   next();
 });
 
-userSchema.pre(/^find/, function (next) {
-  //this points to the current query
-  this.find({ active: { $ne: false } });
-  next();
-});
-
-//Instance Method return true if the two passwords are the same and return false if not
-//userPassword is hased and the candidatePassword is not
-userSchema.methods.correctPassword = async function (
-  candidatePassword,
-  userPassword
-) {
-  return await bcrypt.compare(candidatePassword, userPassword);
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Check if user changed password after the token was issued
 userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   if (this.passwordChangedAt) {
     const changedTimestamp = parseInt(
@@ -94,20 +89,39 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
     console.log(JWTTimestamp, changedTimestamp);
     return JWTTimestamp < changedTimestamp;
   }
-  return false; // False means not changed
+  return false;
 };
 
+// Generate and hash password token
 userSchema.methods.createPasswordResetToken = function () {
+  // Generate token
   const resetToken = crypto.randomBytes(32).toString('hex');
 
+  // Hash token and set to resetPasswordToken field
   this.passwordResetToken = crypto
     .createHash('sha256')
     .update(resetToken)
     .digest('hex');
 
+  // Set expire
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
   return resetToken;
+};
+
+// Generate email confirm token
+userSchema.methods.generateEmailConfirmToken = function (next) {
+  // email confirmation token
+  const confirmationToken = crypto.randomBytes(20).toString('hex');
+
+  this.confirmEmailToken = crypto
+    .createHash('sha256')
+    .update(confirmationToken)
+    .digest('hex');
+
+  const confirmTokenExtend = crypto.randomBytes(100).toString('hex');
+  const confirmTokenCombined = `${confirmationToken}.${confirmTokenExtend}`;
+  return confirmTokenCombined;
 };
 
 const User = mongoose.model('User', userSchema);
