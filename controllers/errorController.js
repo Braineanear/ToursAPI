101b9e66ee2ffda40a58ca34e36b7cff4e0330f8
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2020-12-22 12:46:12 +0200

    update errorController

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -77,8 +77,9 @@ module.exports = (err, req, res, next) => {
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError')
+    if (error.name === 'ValidationError') {
       error = handleValidationErrorDB(error);
+    }
     if (error.name === 'JsonWebTokenError') error = handleJWTError();
     if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
