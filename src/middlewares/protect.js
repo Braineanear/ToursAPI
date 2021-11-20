615bbd5fa430f4ea85a605b8ff22e8ffed8ff5ac
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-11-21 01:51:59 +0200

    fix(bug:middleware): fix a bug in protect middleware change decoded.id with decoded.sub

--- src/middlewares/protect.js
+++ src/middlewares/protect.js
@@ -30,7 +30,7 @@ const protect = catchAsync(async (req, res, next) => {
   const decoded = await promisify(jwt.verify)(token, config.jwt.secret);
 
   // 3) Check if user still exists
-  const currentUser = await User.findById(decoded.id);
+  const currentUser = await User.findById(decoded.sub);
 
   if (!currentUser) {
     return next(
